9e7398c525885bfed325b2ddf8ff0397d2cbce2d
Lenddy <leme2003@gmail.com>
2022-08-07 19:36:36 -0400

    dojo survey model

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using survey_model.Models;
+public class survey:Controller{
+    [HttpGet("")]
+    public IActionResult index(){
+        return View("index");
+    }
+
+    [HttpPost("result")]
+    public IActionResult result(string name, string location, string language, string comment){
+        info  submit = new info(){
+            Name = name,
+            Location = location,
+            Language = language,
+            Comment = comment
+        };
+        return View("result",submit);
+    }
+}
+
---
+++ Models/survey_model.cs
@@ -0,0 +1,8 @@
+namespace survey_model.Models;
+
+public class info{
+    public string Name{get;set;}= null!;
+    public string Location{get;set;} = null!;
+    public string Language{get;set;} = null!;
+    public string? Comment {get;set;}
+}
